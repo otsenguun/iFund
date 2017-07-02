83f602b84fabc1b978abc54ad91be7b3bef03e61
otsenguun <tsenguunkoko@gmail.com>
2017-07-01 21:30:20 -0700

    added .htaccess

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/home', function () {
+    return view('home');
+});
+
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/about', 'AboutController@index')->name('about');
 Route::get('/startups', 'StartupsController@index')->name('startups');
